03463bca8ab22a201aa83fca104f5a5ebdcb7981
icpaintball <isaac.colson@gmail.com>
2019-10-24 11:35:55 -0400

    added spinal case challenge.

---
+++ spinalCase.js
@@ -0,0 +1,24 @@
+function spinalCase(str) {//use regex string manipulation
+
+  var newStr = "";
+  for(let i = 0; i < str.length; i++){
+    let char = str[i];
+    if(char == "-"){
+      newStr += str[i];
+    }else if(char === " " || char === "_"){
+      newStr += "-";
+    }else if(char === char.toUpperCase()){
+      if(str[i-1] != null && (str[i-1] != " " && str[i-1] != "_" && str[i-1] != "-")){
+        newStr += "-";
+      }
+      newStr += char.toLowerCase();
+    }else{
+      newStr += str[i];
+    }
+  }
+  console.log(newStr);
+  return newStr;
+}
+
+spinalCase('This Is Spinal Tap');
+//40 min
